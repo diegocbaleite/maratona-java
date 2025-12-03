2f15d4ef3296b100bdb2c8b49d4a3c1e1e90327e
Diego Leite <diego.leite@sefaz.mt.gov.br>
2025-12-03 14:19:34 -0400

    Aula de numero 07

--- src/main/java/academy/maratona/introducao/aula07/Aula07Arrays02.java
+++ src/main/java/academy/maratona/introducao/aula07/Aula07Arrays02.java
@@ -1,7 +1,18 @@
 package academy.maratona.introducao.aula07;
 
+import java.util.Arrays;
+
 public class Aula07Arrays02 {
     public static void main(String[] args) {
+        // byte, short, int, long, float double 0
+        // char '\u0000' ' ' '
+        // boolean false
+        // String null
 
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kuririm";
+        nomes[2] = "Buma";
+        System.out.println(Arrays.toString(nomes));
     }
 }
