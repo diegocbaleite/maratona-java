cbf99385b1d143d8d17fb779c7a5e7fab3465cb6
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-01-07 07:42:57 -0400

    Aula de numero 04 Orientação Objetos - Exercícios

---
+++ src/main/java/academy/maratona/javacore/introducaoclasses/test/CarroTest.java
@@ -0,0 +1,29 @@
+package academy.maratona.javacore.introducaoclasses.test;
+
+import academy.maratona.javacore.introducaoclasses.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro();
+        carro1.nome = "Civic";
+        carro1.modelo = "Honda";
+        carro1.ano = 2020;
+
+        Carro carro2 = new Carro();
+        carro2.nome = "Corolla";
+        carro2.modelo = "Toyota";
+        carro2.ano = 2022;
+
+        System.out.println(carro1.nome);
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+
+        System.out.println("---------------");
+
+        System.out.println(carro2.nome);
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+    }
+}
+
