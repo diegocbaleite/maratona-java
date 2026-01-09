b6d1f0dcabbfcf91e3c508c2145da67b1ce3807c
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-01-09 14:14:31 -0400

    Aula de número 08 Orientação Objetos - Métodos pt 04 - Retorno pt 02

--- src/main/java/academy/maratona/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/main/java/academy/maratona/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,7 +5,10 @@ import academy.maratona.javacore.Bintroducaometodos.dominio.Calculadora;
 public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-       double resultado = calculadora.divideDoisNumeros(20, 0);
+        double resultado = calculadora.divideDoisNumeros(20, 0);
         System.out.println(resultado);
+        System.out.println(calculadora.divideDoisNumeros02(20, 0));
+        System.out.println("----------------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86, 5);
     }
 }
