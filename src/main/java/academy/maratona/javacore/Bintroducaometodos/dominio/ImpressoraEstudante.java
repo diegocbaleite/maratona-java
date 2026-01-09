67a0a1a1c89746b4444b810f5acdc441967c9784
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-01-09 15:56:38 -0400

    Aula de número 10 Orientação Objetos - Métodos pt 06 - Parâmetros tipo referência

---
+++ src/main/java/academy/maratona/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,11 @@
+package academy.maratona.javacore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprimir(Estudante estudante){
+        System.out.println("----------------");
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.idade);
+    }
+}
---
+++ src/main/java/academy/maratona/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,25 @@
+package academy.maratona.javacore.Bintroducaometodos.test;
+
+import academy.maratona.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.maratona.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante  impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Naruto";
+        estudante01.idade = 12;
+        estudante01.sexo = 'M';
+
+        estudante02.nome= "Sakura";
+        estudante02.idade = 10;
+        estudante02.sexo = 'F';
+
+        impressora.imprimir(estudante01);
+
+        impressora.imprimir(estudante02);
+
+    }
+}
