7a1b118d1c08611a75773651016f1cf1483d361b
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-01-15 12:35:21 -0400

    Aula de número 18 Orientação Objetos - Sobrecarga

--- src/main/java/academy/maratona/javacore/Dconstrutores/dominio/Anime.java
+++ src/main/java/academy/maratona/javacore/Dconstrutores/dominio/Anime.java
@@ -5,17 +5,23 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime(String nome, String tipo, int episodios, String genero) {
-        System.out.println("Dentro do construtor");
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime() {
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
 
+    public Anime() {
+        System.out.println("Dentro do construtor sem argumentos");
     }
 
     public void imprimir() {
@@ -23,6 +29,15 @@ public class Anime {
         System.out.println("Tipo: " + tipo);
         System.out.println("Episodios: " + episodios);
         System.out.println("Genero: " + genero);
+        System.out.println("Estudio: " + estudio);
+    }
+
+    public String getEstudio() {
+        return estudio;
+    }
+
+    public void setEstudio(String estudio) {
+        this.estudio = estudio;
     }
 
     public void setNome(String nome) {
--- src/main/java/academy/maratona/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/main/java/academy/maratona/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,8 @@ import academy.maratona.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Boruto", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Boruto", "TV", 12, "Ação","Production IG");
         anime.imprimir();
-        anime2.imprimir();
+
     }
 }
