e29c970852486f83674189c16f2cc6134af20d4b
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-01-26 09:57:39 -0400

    Aula de número 21 Orientação Objetos - Métodos estáticos

--- src/main/java/academy/maratona/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/main/java/academy/maratona/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,14 +4,14 @@ import academy.maratona.javacore.Fmodificadorestatico.domain.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite(180);
+
         Carro c1 = new Carro("BMW", 280);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 279);
 
-        Carro.velocidadeLimite = 180;
-
-        c1.imprimir();
-        c2.imprimir();
-        c3.imprimir();
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
     }
 }
