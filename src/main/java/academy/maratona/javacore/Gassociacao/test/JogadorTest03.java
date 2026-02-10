ea471d10385580e1aed7700e76a379283a98c68c
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-02-10 09:27:00 -0400

    Aula de número 26 Orientação Objetos - Associação pt 04 - Associação bidirecional.

---
+++ src/main/java/academy/maratona/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,24 @@
+package academy.maratona.javacore.Gassociacao.test;
+
+import academy.maratona.javacore.Gassociacao.dominio.Jogador;
+import academy.maratona.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafú");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador,  jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+
+        time.setJogadores(jogadores);
+
+        System.out.println("---Jogador---");
+        jogador.imprimir();
+
+        System.out.println("---Time---");
+        time.imprime();
+    }
+}
