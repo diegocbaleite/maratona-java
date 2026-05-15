b2e64b0160ffe5f935d6d750c76c40e51c16483d
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-05-15 11:42:29 -0400

    Aula de número 35 Orientação Objetos Sobrescrita do método toString

---
+++ src/main/java/academy/maratona/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.maratona.javacore.Isobrescrita.test;
+
+import academy.maratona.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+
+        System.out.println(anime);
+    }
+}
