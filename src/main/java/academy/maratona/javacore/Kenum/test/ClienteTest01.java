c9f9d49aac307de8e011f64581004b4c8fe41efa
Diego Leite <diego.leite@sefaz.mt.gov.br>
2026-08-04 08:38:33 -0400

    Aula de número 40 Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- src/main/java/academy/maratona/javacore/Kenum/test/ClienteTest01.java
+++ src/main/java/academy/maratona/javacore/Kenum/test/ClienteTest01.java
@@ -2,16 +2,19 @@ package academy.maratona.javacore.Kenum.test;
 
 import academy.maratona.javacore.Kenum.dominio.Cliente;
 import academy.maratona.javacore.Kenum.dominio.TipoCliente;
+import academy.maratona.javacore.Kenum.dominio.TipoPagamento;
 
-import static academy.maratona.javacore.Kenum.dominio.Cliente.TipoPagamento.*;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
 
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, DEBITO);
-        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, CREDITO);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
